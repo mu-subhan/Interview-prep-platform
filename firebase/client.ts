e4906428104905eb7426aabc10dfea3a548e6466
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-04-02 23:38:39 +0500

    updated

--- firebase/client.ts
+++ firebase/client.ts
@@ -6,8 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//  Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
